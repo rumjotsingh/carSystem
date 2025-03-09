4e592017dddf5fa268a02f6ecc79e6bc07971a04
rumjotsingh <155811126+rumjotsingh@users.noreply.github.com>
2025-03-09 17:16:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,12 @@ import cors from "cors";
 import userRoutes from "./routes/user.routes.js";
 import carRoutes from "./routes/carListing.routes.js";
 import reviewRoutes from "./routes/review.routes.js";
+import axios from "axios";
+import feedbackesRoutes from "./routes/feedback.routes.js";
+import bodyParser from "body-parser";
 dotenv.config();
 const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,6 +28,8 @@ async function main() {
 app.listen(process.env.PORT, (req, res) => {
   console.log("the sever is listening up port ", process.env.PORT);
 });
+
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/cars", carRoutes);
+app.use("/api/v1/feedbacks", feedbackesRoutes);
 app.use("/api/v1/reviews", reviewRoutes);
